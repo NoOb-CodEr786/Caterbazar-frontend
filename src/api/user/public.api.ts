1833a3165f743e9068bd87bb2c6e42d53b27e16c
NoOb-CodEr786 <sahusurya1580@gmail.com>
2025-11-29 00:43:25 +0530

    Integrate all the apis

--- src/api/user/public.api.ts
+++ src/api/user/public.api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api/v1';
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export interface VendorSearchParams {
   vendorCategory?: string;
